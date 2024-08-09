91f819f325564c1b96c0c11f592d172c9cf9dfcf
Klaas Jan Wientjes <wientjes@gmail.com>
2024-08-09 23:06:07 +0200

    Changed message

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,6 +1,6 @@
-// Learning C++ 
+// Learning C++
 // Exercise 00_03
-// Using the exercise files on GitHub, by Eduardo Corpeño 
+// Using the exercise files on GitHub, by Eduardo Corpeño
 
 #include <iostream>
 
@@ -11,11 +11,11 @@ int main(){
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
-    
+
     result = num_1 + num_2;
-    
+
     std::cout << "The result of the addition is " << result << std::endl;
-    
+
     std::cout << std::endl << std::endl;
     return (0);
 }
