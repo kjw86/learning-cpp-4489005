724ac09e37d564fc5838afcd3046fa4c88b4435c
Klaas Jan Wientjes <wientjes@gmail.com>
2024-08-11 08:58:52 +0200

    Input name

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -3,12 +3,14 @@
 // Hello World, by Eduardo Corpeño
 // modified by kjw
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello There!" << std::endl;
+int main(){
+    std::string str_name;
+    std::cout << "Hi There please enter your first name: " << std::flush;
+    std::cin >> str_name;
+    std::cout << "Hello " << str_name << ", how are you today?" << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
 }
-
