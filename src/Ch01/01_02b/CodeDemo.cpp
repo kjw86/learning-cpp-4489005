7d3302d8f2061f6f0bbb6b6b883ed0fab6f57f68
Klaas Jan Wientjes <wientjes@gmail.com>
2024-08-11 08:11:05 +0200

    included author KJW

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
-// Learning C++ 
+// Learning C++
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Eduardo Corpeño
+// modified by kjw
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello There!" << std::endl;
+    return 0;
+}
+
